256c2bc289aafcd17893f87bf2c4944833f6aa23
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-09-06 00:00:58 -0300

    feat: added migrations for the tables

--- database/migrations/2019_09_06_021011_create_curso.php
+++ database/migrations/2019_09_06_021011_create_curso.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCurso extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('curso', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_instituicao')->index();
+            $table->string('nome', 50);
+            $table->string('duracao', 20);
+            $table->tinyInteger('status');
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('curso');
     }
 }
